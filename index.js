d419a62bef6440d4c28b55a70b182cb99e7901fa
Adrian Higgins <adrianhiggins@Adrians-iMac.local>
2018-12-27 11:44:56 -0500

    Updates to the Jobs Loader, Configuration, Conduct Commands (Added Cron Start, SymLinker, and Default Public File Storage), and continuing to plug away on MongoDB Stuff.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const Route                = require("./lib/Route/Route");
-const Shock                = require("./lib/Shock/Shock");
-const MySql                = require("./lib/MySql/MySql");
-const MongoDb              = require("./lib/MongoDb/MongoDb");
-const Schema               = require("./lib/Migration/src/Schema");
-const Migrate              = require("./lib/Migration/bin/Migration");
+const Broadcast            = require("./lib/Broadcast/Broadcast");
+const Event                = require("./lib/Broadcast/Event");
+const Channel              = require("./lib/Broadcast/Channel");
 const ModelComponent       = require("./lib/Conduct/components/Model");
 const ControllerComponent  = require("./lib/Conduct/components/Controller");
 const MigrationComponent   = require("./lib/Conduct/components/Migration");
-const Job                  = require("./lib/Job/Job");
-const JobLoader            = require("./lib/Job/bin/JobLoader");
-const Encryption           = require("./lib/Encryption/Encryption");
-const Csv                  = require("./lib/Csv/Csv");
-const Config               = require("./lib/Config/Config");
+const Structure            = require("./lib/Conduct/structure/Structure");
+const App                  = require("./lib/Config/Config");
 const Init                 = require("./lib/Config/Init");
 const MySqlConfig          = require("./lib/Config/MySqlConfig");
 const CsvConfig            = require("./lib/Config/CsvConfig");
 const MongoDbConfig        = require("./lib/Config/MongoDbConfig");
-const Broadcast            = require("./lib/Broadcast/Broadcast");
-const Event                = require("./lib/Broadcast/Event");
-const Channel              = require("./lib/Broadcast/Channel");
-const ErrorLog             = require("./lib/Errors/src/ErrorLog");
-const Model                = require("./lib/Model/Model");
 const Controller           = require("./lib/Controller/Controller");
+const Csv                  = require("./lib/Csv/Csv");
+const Encryption           = require("./lib/Encryption/Encryption");
+const ErrorLog             = require("./lib/Errors/src/ErrorLog");
 const File                 = require("./lib/File/File");
+const Job                  = require("./lib/Job/Job");
+const JobLoader            = require("./lib/Job/bin/JobLoader");
+const Schema               = require("./lib/Migration/src/Schema");
+const Migrate              = require("./lib/Migration/bin/Migration");
+const Model                = require("./lib/Model/Model");
+const MongoDb              = require("./lib/MongoDb/MongoDb");
+const MySql                = require("./lib/MySql/MySql");
+const Route                = require("./lib/Route/Route");
+const Shock                = require("./lib/Shock/Shock");
 
 module.exports = { Route, Shock, MySql, MongoDb, Schema, Migrate, Job, JobLoader,
-Encryption, Csv, Config, Init, MySqlConfig, CsvConfig, MongoDbConfig, Broadcast, Event,
+Encryption, Csv, App, Init, MySqlConfig, CsvConfig, MongoDbConfig, Broadcast, Event,
 Channel, ErrorLog, Model, Controller, ModelComponent, ControllerComponent, MigrationComponent,
-File };
+File, Structure };
